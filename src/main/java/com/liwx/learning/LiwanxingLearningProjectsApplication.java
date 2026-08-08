ddd28fc68293be4f099db098f672aaffcfb6fdb3
liwanxing <liwanxing@users.noreply.github.com>
2026-08-09 00:43:24 +0800

    feat: 初始化Spring Boot项目，添加HelloController接口

---
+++ src/main/java/com/liwx/learning/LiwanxingLearningProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.liwx.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiwanxingLearningProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiwanxingLearningProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liwx/learning/LiwanxingLearningProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liwx.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiwanxingLearningProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
